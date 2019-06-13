10c124b7db3eeb854b615ab46ef369d1b281985e
olegk <olegk@DESKTOP-FEKJIGM>
2019-06-13 23:42:55 +0300

    Test details history

---
+++ Portal/Portal/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Emploees()
+        {
+            return View();
+        }
+
+        public ActionResult Tests()
+        {
+            return View();
+        }
+
+        public ActionResult ViewTest()
+        {
+            return View();
+        }
+
+        public ActionResult EditTest()
+        {
+            return View();
+        }
+
+        public ActionResult EditQuestion()
+        {
+            return View();
+        }
+
+        public ActionResult EditAnswer()
+        {
+            return View();
+        }
+    }
+}
